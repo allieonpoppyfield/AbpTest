2b93096c0a4e2e0a2b9d86fe7ef74f4baab1b934
allie <allie@DESKTOP-BPD4NLJ>
2023-02-08 20:17:12 +0300

    Добавьте файлы проекта.

---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'AbpTest',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44326/',
+    redirectUri: baseUrl,
+    clientId: 'AbpTest_App',
+    responseType: 'code',
+    scope: 'offline_access AbpTest',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44326',
+      rootNamespace: 'AbpTest',
+    },
+  },
+} as Environment;
